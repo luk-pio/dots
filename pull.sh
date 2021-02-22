653ed9cc61a1f9ba8216281c98d2024c28fe7747
luk-pio <luke.piotrak@gmail.com>
2021-02-22 23:52:08 +0100

    Fix tags

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo ansible-pull -U https://github.com/luk-pio/dots.git -C main -t core home
+sudo ansible-pull -U https://github.com/luk-pio/dots.git -C main -t core -t home
